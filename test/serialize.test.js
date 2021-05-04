174a89e6592005563e5bd5c8962490e3fc516dda
Émile Perron <contact@emileperron.com>
2021-05-04 11:45:32 -0400

    Removed bloat from serialization unit test

--- test/serialize.test.js
+++ test/serialize.test.js
@@ -10,61 +10,9 @@ import pkg_jsdom from "jsdom";
 const { JSDOM } = pkg_jsdom;
 import "jsdom-global";
 
-// DataTree for tree management
-import pkg_datatree from "data-tree";
-const { dataTree } = pkg_datatree;
-
 // Local imports
-import { getVerdict } from "../index.mjs";
-import {
-    Addition,
-    BackgroundColor,
-    BackgroundImage,
-    BorderColor,
-    BorderRadius,
-    BorderStyle,
-    BorderWidth,
-    CssPropertyFunction,
-    Color,
-    CompoundDesignator,
-    ComposedFunction,
-    ConstantDesignator,
-    ConstantFunction,
-    ComposedFunctionValue,
-    DimensionHeight,
-    Display,
-    DimensionWidth,
-    ElementAttributeValue,
-    EnumeratedValue,
-    FindBySelector,
-    Float,
-    FontFamily,
-    FontSize,
-    FontWeight,
-    GreaterThan,
-    GreaterOrEqual,
-    IsEqualTo,
-    MarginTop,
-    MarginBottom,
-    MarginRight,
-    MarginLeft,
-    ObjectNode,
-    Opacity,
-    PathValue,
-    PaddingTop,
-    PaddingBottom,
-    PaddingRight,
-    PaddingLeft,
-    Position,
-    ReturnValue,
-    TestCondition,
-    Tracer,
-    UniversalQuantifier,
-    Visibility,
-    Zindex
-} from "../index.mjs";
+import { ComposedFunctionValue, EnumeratedValue } from "../index.mjs";
 import { Serialization } from "../index.mjs";
-import { expect_to_throw } from "./test-util.mjs";
 
 
 describe("Test Serialization", () => {
@@ -108,101 +56,12 @@ describe("Test Serialization", () => {
 
     it("should be return true when input json is object", function () {
         expect(expectedJsonExample).to.be.an("object")
-    })
+    });
 
     it("should deserialize and serialize an object json ", function () {
         deserialized = sampleserizalisation.deserialize(expectedJsonExample)
         const serialized = sampleserizalisation.serialize(deserialized)
         expect(serialized).to.be.deep.equal(expectedJsonExample)
-    })
-
-    it("Test opacity from json structure", async () => {
-        var j = {
-            "name": "UniversalQuantifier",
-            "contents": [
-                "$x",
-                {
-                    "name": "FindBySelector",
-                    "contents": [
-                        "#h2"
-                    ]
-                },
-                {
-                    "name": "ComposedFunction",
-                    "contents": [{
-                        "name": "GreaterThan",
-                        "contents": []
-                    },
-                    {
-                        "name": "ComposedFunction",
-                        "contents": [{
-                            "name": "Opacity",
-                            "contents": []
-                        },
-                            "$x"
-                        ]
-                    },
-                    {
-                        "name": "ConstantFunction",
-                        "contents": [0.9]
-                    }
-                    ]
-                }
-            ]
-        }
-        var dom = await load_dom("./test/pages/stub-1.html");
-        var body = dom.window.document.body;
-        deserialized = sampleserizalisation.deserialize(j);
-        var cond = new TestCondition("h2's opacity is equal to 1", deserialized);
-        var tree = getVerdict(body, cond);
-        //console.log(tree);
-        //console.log('============ serialization ==============')
-        //var f = new Serialization().serialize(des);
-        //console.log(f)
-    });
-    it("Test Background-color from json structure", async() => {
-        var j = {
-            "name": "UniversalQuantifier",
-            "contents": [
-                "$x",
-                {
-                    "name": "FindBySelector",
-                    "contents": [
-                        "#p1"
-                    ]
-                },
-                {
-                    "name": "ComposedFunction",
-                    "contents": [{
-                            "name": "IsEqualTo",
-                            "contents": []
-                        },
-                        {
-                            "name": "ComposedFunction",
-                            "contents": [{
-                                    "name": "BackgroundColor",
-                                    "contents": []
-                                },
-                                "$x"
-                            ]
-                        },
-                        {
-                            "name": "ConstantFunction",
-                            "contents": ["blue"]
-                        }
-                    ]
-                }
-            ]
-        }
-        var dom = await load_dom("./test/pages/stub-1.html");
-        var body = dom.window.document.body;
-        var deserialized = sampleserizalisation.deserialize(j);
-        var cond = new TestCondition("p1's background-color is  blue", deserialized);
-        var tree = getVerdict(body, cond);
-        // console.log(tree);
-        // console.log('============ serialization ==============')
-        // var f = new Serialization().serialize(des);
-        //console.log(f)
     });
 
     it("Test GreaterThan from json structure", function () {
@@ -213,13 +72,8 @@ describe("Test Serialization", () => {
         deserialized = sampleserizalisation.deserialize(j);
         var gt = deserialized.evaluate(4, 3)
         expect(gt.getValue()).to.be.true;
-
-        //console.log('============ serialization Test GreaterThan ==============')
-        //var f = new Serialization().serialize(s);
-        //console.log(f)
-        //expect(j).to.be.deep.equal(f)
-
     });
+
     it("Test FindbySelector from json structure", async () => {
         var j = {
             "name": "FindBySelector",
@@ -230,22 +84,15 @@ describe("Test Serialization", () => {
         var dom = await load_dom("./test/pages/stub-1.html");
         var body = dom.window.document.body;
         deserialized = sampleserizalisation.deserialize(j);
-        //console.log(s)
-        //var cond = new TestCondition("h2's Selector", s);
         var v = deserialized.evaluate(body);
         var elems = v.getValue();
-        //console.log(elems)
+        
         expect(Array.isArray(elems)).to.be.true;
         expect(elems.length).to.equal(1);
         var e1 = elems[0];
         expect(e1).to.be.an.instanceof(EnumeratedValue);
         var v1 = e1.getValue();
         expect(v1.tagName).to.equal("H2");
-
-        //console.log('============ serialization Test FindbySelector ==============')
-        //var f = new Serialization().serialize(s);
-        //console.log(f)
-
     });
 
     it("Test Composed Function from json structure", function () {
@@ -260,17 +107,11 @@ describe("Test Serialization", () => {
             ]
         }
         var deserialized = sampleserizalisation.deserialize(j);
-        //console.log(cf)
         expect(deserialized.getArity()).to.equal(0);
         var r = deserialized.evaluate();
         expect(r).to.be.an.instanceof(ComposedFunctionValue);
         var v = r.getValue();
         expect(v).to.equal(5);
-
-        //console.log('============ serialization Test Composed Function ==============')
-        //var f = new Serialization().serialize(cf);
-        //console.log(f)
-
     });
 
 });
